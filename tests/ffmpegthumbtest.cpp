1574fbefd9895fc7e1d1b6771bae6aae2ec970de
Andreas Scherf <ascherfy@googlemail.com>
2010-06-07 20:50:26 +0000

    ffmpegthumbs: Fixed a comment.
    svn path=/trunk/KDE/kdemultimedia/ffmpegthumbs/; revision=1135646

--- tests/ffmpegthumbtest.cpp
+++ tests/ffmpegthumbtest.cpp
@@ -1,5 +1,4 @@
-
-//    Copyright (C) 2010 Dirk Andreas Scherf <ascherfy@gmail.com>
+//    Copyright (C) 2010 Andreas Scherf <ascherfy@gmail.com>
 //
 //    This program is free software; you can redistribute it and/or modify
 //    it under the terms of the GNU General Public License as published by
